2aeb17d7befcaff5d9662b8e4cde57d58ba75b39
nicod12 <dumenicolas45@gmail.com>
2023-09-13 06:05:39 -0300

    upd2

--- src/components/stats/StatsComponent.tsx
+++ src/components/stats/StatsComponent.tsx
@@ -2,17 +2,43 @@ import './stats.css'
 
 export const StatsComponent = (): JSX.Element => {
   return (
-    <section className='stats-container'>
+   <>
+    <section className='central-area'>
      <section className='match-result'>
-        <article>
-        </article>
-        <article className='score'>
+        <div className='team-left'>
           <img src="https://i.postimg.cc/Bn6xYPwQ/Team-1.png" alt="team1" />
-        </article>
-        <article>
-        <img src="https://i.postimg.cc/HxQ5cwj3/Team-2.png" alt="team2" />
+        </div>
+
+        <section className='score'>
+          <section>
+            <article className='crumbs-score'>
+              <div className="score-layout">
+                <div className="crumbs-score">
+                  <div className="line"></div>
+                  <div className="frame">
+                    <div className="text-score">2</div>
+                    <div className="separator"></div>
+                    <div className="text-score">1</div>
+                  </div>
+                  <div className="r-line"></div>
+                </div>
+                <div className="finished">
+                  <h2 className='text-finished'>+3 points</h2>
+                </div>
+                <div className='training-token'>
+                  <img src="https://i.postimg.cc/CKx0bgRr/Training-token.png" alt="token" />
+                </div>
+              </div>
+            </article>
+          </section>
+        </section>
+
+        <article className='team-right'>
+          <img src="https://i.postimg.cc/HxQ5cwj3/Team-2.png" alt="team2" />
         </article>
      </section>
     </section>
+     <img className='barra' src="https://i.postimg.cc/VLz1RmtB/Separador2.png" alt="separador2" />
+   </>
   )
 }
